4516d8f71610a8255310675a5ad0706914e069d2
Jashan <jashandeep4329@gmail.com>
2025-09-17 21:32:07 +0530

    Refactor GameBoard and GameController modules to remove hardcoded moves and enhance game logic

--- gameBoard.js
+++ gameBoard.js
@@ -16,15 +16,7 @@ const GameBoard = (() => {
         }
 })();
 
-GameBoard.updateBoard(5, "X");
-GameBoard.updateBoard(6, "O");
-GameBoard.updateBoard(4, "X");
-GameBoard.updateBoard(0, "O");
-GameBoard.updateBoard(3, "X");
-GameBoard.updateBoard(1, "O");
-GameBoard.updateBoard(2, "X");
-GameBoard.updateBoard(7, "O");
-GameBoard.updateBoard(8, "X");
 
 
-console.log(GameBoard.getBoard());
+
+export default GameBoard;
--- gameController.js
+++ gameController.js
@@ -1,4 +1,5 @@
 import player from "./player.js"
+import GameBoard from "./gameBoard.js";
 
 const GameController = () => {
 
@@ -7,6 +8,26 @@ const GameController = () => {
  
     console.log(player1);
     console.log(player2);
+
+    console.log("GAME START!!!");
+
+    currentPlayer = player1;
+
+    const playMove = (index) => {
+
+        if(GameBoard.getBoard()[index] !== "") return;
+
+        GameBoard.updateBoard(index,currentPlayer.marker)
+
+
+        if(currentPlayer === player1) {
+            currentPlayer = player2;
+        } else{
+            currentPlayer = player1
+        }
+    } 
+
+
 }
 
 GameController();
